841f554794af9e9fba2a42dc6d2d7528bec90618
jonahhwest0408 <jonah.hwest0408@gmail.com>
2024-04-04 10:19:55 -0700

    Button controls bug, Styling, and Adding additional songs/ content

--- finalproject/index.js
+++ finalproject/index.js
@@ -1,15 +1,51 @@
-let songs = [];
+let songs;
+let song;
+let audio;
+let currentSongIndex = 0;
 
-function fetchSongData() {
-    fetch('songs.json')
-        .then(response => response.json())
-        .then(data => {
-            songs = data;
+const fetchSongData = async () => {
+    try {
+        const response = await fetch('https://mocki.io/v1/2711d74e-c257-4b3a-a214-a2f47abe7f96');
+        if (!response.ok) {
+            throw new Error('Failed to fetch song data');
+        }
+        songs = await response.json();
+        if (songs.length > 0) {
             loadSong(0); 
-        })
-        .catch(error => {
-            console.error('Error fetching song data:', error);
-        });
+        } else {
+            console.error('No songs found in the array');
+        }
+    } catch (error) {
+        console.error('Error fetching song data:', error);
+    }
+};
+
+function loadSong(songIndex) {
+    const selectedSong = songs[songIndex];
+    song = new Audio(selectedSong.source);
+    audio = document.getElementById('song');
+    audio.src = selectedSong.source;
+    displaySongInfo(selectedSong);
+
+    song.addEventListener('timeupdate', function() {
+        const currentTime = song.currentTime;
+        const duration = song.duration;
+        const progressPercentage = (currentTime / duration) * 100;
+        progress.value = progressPercentage;
+    });
+
+    song.addEventListener('ended', function() {
+        nextSong();
+    });
+
+    progress.addEventListener('input', function() {
+        const seekTime = (progress.value / 100) * song.duration;
+        song.currentTime = seekTime;
+    });
+
+    progress.addEventListener('change', function() {
+        song.play(); 
+    });
 }
 
 function playPause() {
@@ -17,12 +53,33 @@ function playPause() {
         song.pause();
         ctrlIcon.classList.remove("fa-pause");
         ctrlIcon.classList.add("fa-play");
-    }
-    else {
+    } else {
         song.play();
         ctrlIcon.classList.add("fa-pause");
         ctrlIcon.classList.remove("fa-play");
     }
+}
+
+function nextSong() {
+    song.pause(); // Pause the current song
+    currentSongIndex++;
+    
+    if (currentSongIndex >= songs.length) {
+        currentSongIndex = 0;
+    }
+    
+    loadSong(currentSongIndex);
+}
+
+function previousSong() {
+    song.pause(); // Pause the current song
+    currentSongIndex--;
+    
+    if (currentSongIndex < 0) {
+        currentSongIndex = songs.length - 1;
+    }
+    
+    loadSong(currentSongIndex);
 }
 
 function displaySongInfo(song) {
@@ -36,35 +93,6 @@ function displaySongInfo(song) {
     }
 }
 
-
-function loadSong(songIndex) {
-    const selectedSong = songs[songIndex];
-    displaySongInfo(selectedSong);
-    song.src = selectedSong.source;
-    song.play();
-    ctrlIcon.classList.add("fa-pause");
-}
-
-loadSong(0);
-
-song.onplaying = function() {
-    setInterval(() => {
-        progress.value = song.currentTime;
-    }, 500);
-};
-
-progress.onchange = function() {
-    song.currentTime = progress.value;
-};
-
-function nextSong() {
-    currentSongIndex++;
-    
-    if (currentSongIndex >= songs.length) {
-        currentSongIndex = 0;
-    }
-    
-    loadSong(currentSongIndex);
-}
+const progress = document.getElementById('progress');
 
 fetchSongData();
